6312d84f078fcaa19d1a6332cbf7499f8e170ebe
ziovio <anderkoval3@gmail.com>
2021-05-11 22:52:50 +0300

    run-db.sh file change place

---
+++ term2/coursework/run-db.sh
@@ -0,0 +1,6 @@
+#mongod --dbpath ./data/db/db1 --port 27001 --replSet replica
+#mongod --dbpath ./data/db/db2 --port 27002 --replSet replica
+#mongod --dbpath ./data/db/db3 --port 27003 --replSet replica
+
+#connect to server
+#mongo --host 127.0.0.1 --port 27002
